fff141e2a8445fd94d7d12fee8d0519a192000b8
Tyrone Bowman <88301291+TytheBowman@users.noreply.github.com>
2023-04-12 17:21:32 -0400

    completed what was asked in category and index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag',
+  foreignKey: 'tag_id',
+
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  foreignKey: 'product_id',
+
+});
 
 module.exports = {
   Product,
